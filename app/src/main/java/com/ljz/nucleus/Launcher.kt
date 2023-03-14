c54790a509807b3310e6511fc8a25ec55ea2ef2c
LJZ <leonzapke@gmail.com>
2023-03-14 07:54:14 +0100

    added Home.kt; Updated Login.kt

--- app/src/main/java/com/ljz/nucleus/Launcher.kt
+++ app/src/main/java/com/ljz/nucleus/Launcher.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
@@ -26,11 +27,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import com.ljz.nucleus.ui.theme.NucleusTheme
+
+private lateinit var auth: FirebaseAuth
 
 class Launcher : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
         setContent {
             NucleusTheme {
                 ShowWelcomeUI()
@@ -41,61 +53,76 @@ class Launcher : ComponentActivity() {
 
 @Composable
 fun ShowWelcomeUI() {
-// A surface container using the 'background' color from the theme
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
             .background(MaterialTheme.colorScheme.background)
     ) {
-        ConstraintLayout {
-            val (welcomeText1, welcomeText2, loginButton) = createRefs()
+        val user = auth.currentUser
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        if(user != null) {
             val context = LocalContext.current
             val activity = (LocalContext.current as? Activity)
+            context.startActivity(Intent(context, Home::class.java))
+            activity?.finish()
+        } else {
+            ConstraintLayout {
+                val (welcomeText1, welcomeText2, loginButton) = createRefs()
+                val context = LocalContext.current
+                val activity = (LocalContext.current as? Activity)
 
-            Text("Willkommen bei Nucleus!",
-                style = TextStyle(
-                    textAlign = TextAlign.Left,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.ExtraBold
-                ),
-                modifier = Modifier.constrainAs(welcomeText1) {
-                    top.linkTo(parent.top, margin = 15.dp)
-                    absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
-                    absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
-                    width = Dimension.fillToConstraints
-                })
-
-            Text(
-                "Unsere Social-Media-Plattform ist darauf ausgelegt, dass du dich sicher und wohl fühlen kannst, während du deine Beiträge erstellst und mit Freunden teilst." +
-                        "\nWir legen besonderen Wert auf Datenschutz und Individualität und bieten ein einzigartiges Erlebnis, das auf deine Interessen und Vorlieben zugeschnitten ist.",
-                style = TextStyle(
-                    fontSize = 18.sp
-                ),
-                modifier = Modifier.constrainAs(welcomeText2) {
-                    top.linkTo(welcomeText1.bottom, margin = 10.dp)
-                    absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
-                    absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            Button(
-                onClick = {
-                    context.startActivity(Intent(context, Login::class.java))
-                    activity?.finish()
-                },
-                modifier = Modifier.constrainAs(loginButton) {
-                    absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
-                    absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
-                    bottom.linkTo(parent.bottom, margin = 15.dp)
-                    width = Dimension.fillToConstraints
-                }
-            ) {
-                Text(
-                    "Anmelden",
-                    style = TextStyle(fontSize = 15.sp)
+                Text("Willkommen bei Nucleus!",
+                    style = TextStyle(
+                        textAlign = TextAlign.Left,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.ExtraBold
+                    ),
+                    modifier = Modifier
+                        .constrainAs(welcomeText1) {
+                            top.linkTo(parent.top, margin = 15.dp)
+                            absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
+                            absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
+                            width = Dimension.fillToConstraints
+                        }
                 )
+
+                Text(
+                    "Unsere Social-Media-Plattform ist darauf ausgelegt, dass du dich sicher und wohl fühlen kannst, während du deine Beiträge erstellst und mit Freunden teilst." +
+                            "\nWir legen besonderen Wert auf Datenschutz und Individualität und bieten ein einzigartiges Erlebnis, das auf deine Interessen und Vorlieben zugeschnitten ist.",
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    ),
+                    modifier = Modifier.constrainAs(welcomeText2) {
+                        top.linkTo(welcomeText1.bottom, margin = 10.dp)
+                        absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
+                        absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
+                        width = Dimension.fillToConstraints
+                    }
+                )
+
+                Button(
+                    onClick = {
+                        context.startActivity(Intent(context, Login::class.java))
+                        activity?.finish()
+                    },
+                    modifier = Modifier.constrainAs(loginButton) {
+                        absoluteLeft.linkTo(parent.absoluteLeft, margin = 15.dp)
+                        absoluteRight.linkTo(parent.absoluteRight, margin = 15.dp)
+                        bottom.linkTo(parent.bottom, margin = 15.dp)
+                        width = Dimension.fillToConstraints
+                    }
+                ) {
+                    Text(
+                        "Anmelden",
+                        style = TextStyle(fontSize = 15.sp)
+                    )
+                }
             }
         }
     }
